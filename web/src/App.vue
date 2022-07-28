23c862ecbd1e404b75e029ac81f988c048684bbc
LonelyLiar <oliver975@163.com>
2022-07-28 16:19:52 +0800

    4.2 配置Mysql与注册登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,9 +7,56 @@
 import NavBar from './components/NavBar.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+
+//import $ from 'jquery'
 export default{
   components:{
     NavBar
+  },
+  setup(){
+    // $.ajax({
+    //   url:"http://127.0.0.1:3000/user/account/token/",
+    //   type:"post",
+    //   data:{
+    //     username:"fkh",
+    //     password:"pfkh",
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // });
+
+    // $.ajax({
+    //   url:"http://127.0.0.1:3000/user/account/info/",
+    //   type:"get",
+    //   headers:{
+    //     Authorization:"Bearer "+"eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI0ZTI1NzU3ZjBlZmE0ZmI0YTgzMTkwN2ZjYTQ5MzU0OSIsInN1YiI6IjEiLCJpc3MiOiJzZyIsImlhdCI6MTY1ODkxMDAxMiwiZXhwIjoxNjYwMTE5NjEyfQ.wLJ9iOewetSEwLfxCJFjW326OwyXhEFtGrWPVsq5wyY"
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // });
+    // $.ajax({
+    //   url:"http://127.0.0.1:3000/user/account/register/",
+    //   type:"post",
+    //   data:{
+    //     username:"fkh",
+    //     password:"123",
+    //     confirmedPassword:"123",
+    //   },
+    //   success(resp){
+    //     console.log(resp);
+    //   },
+    //   error(resp){
+    //     console.log(resp);
+    //   }
+    // })
   }
 }
 </script>
