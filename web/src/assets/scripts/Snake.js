2f195c0576500a14ddeb943dbfa9dc5cee68d141
LonelyLiar <oliver975@163.com>
2022-09-01 18:50:52 +0800

    6.2 实现微服务：匹配系统（中）

--- web/src/assets/scripts/Snake.js
+++ web/src/assets/scripts/Snake.js
@@ -21,20 +21,21 @@ export class Snake extends AcGameObject {
 
         this.step = 0;  // 表示回合数
         this.eps = 1e-2;  // 允许的误差
+
         this.eye_direction = 0;
-        if(this.id === 1) this.eye_direction = 2; //左下角的蛇初始朝上，右上蛇朝下
-        
-        this.eye_dx = [ //蛇眼睛不同方向的x的偏移量
-            [-1,1],
-            [1,1],
-            [-1,1],
-            [-1,-1],
-        ]
-        this.eye_dy = [ //蛇眼睛不同方向的y偏移量
-            [-1,-1],
-            [-1,1],
-            [1,1],
-            [1,-1],
+        if (this.id === 1) this.eye_direction = 2;  // 左下角的蛇初始朝上，右上角的蛇朝下
+
+        this.eye_dx = [  // 蛇眼睛不同方向的x的偏移量
+            [-1, 1],
+            [1, 1],
+            [1, -1],
+            [-1, -1],
+        ];
+        this.eye_dy = [  // 蛇眼睛不同方向的y的偏移量
+            [-1, -1],
+            [-1, 1],
+            [1, 1],
+            [1, -1],
         ]
     }
 
@@ -45,12 +46,11 @@ export class Snake extends AcGameObject {
     set_direction(d) {
         this.direction = d;
     }
-    
-    check_tail_increasing(){  //检测当前回合，蛇的长度是否增加
-        if(this.step <=10 ) return true;
-        if(this.step % 3 === 1) return true;
-        return false;
 
+    check_tail_increasing() {  // 检测当前回合，蛇的长度是否增加
+        if (this.step <= 10) return true;
+        if (this.step % 3 === 1) return true;
+        return false;
     }
 
     next_step() {  // 将蛇的状态变为走下一步
@@ -65,10 +65,6 @@ export class Snake extends AcGameObject {
         for (let i = k; i > 0; i -- ) {
             this.cells[i] = JSON.parse(JSON.stringify(this.cells[i - 1]));
         }
-        if(!this.gamemap.check_valid(this.next_cell)){ //蛇死了
-            this.status = "die";
-        }
-      
     }
 
     update_move() {
@@ -80,24 +76,23 @@ export class Snake extends AcGameObject {
             this.cells[0] = this.next_cell;  // 添加一个新蛇头
             this.next_cell = null;
             this.status = "idle";  // 走完了，停下来
-            if(!this.check_tail_increasing()) { //蛇不变长
+
+            if (!this.check_tail_increasing()) {  // 蛇不变长
                 this.cells.pop();
             }
-           
         } else {
             const move_distance = this.speed * this.timedelta / 1000;  // 每两帧之间走的距离
             this.cells[0].x += move_distance * dx / distance;
             this.cells[0].y += move_distance * dy / distance;
 
-            if(!this.check_tail_increasing()){
+            if (!this.check_tail_increasing()) {
                 const k = this.cells.length;
-                const tail = this.cells[k-1],tail_target = this.cells[k-2];
+                const tail = this.cells[k - 1], tail_target = this.cells[k - 2];
                 const tail_dx = tail_target.x - tail.x;
                 const tail_dy = tail_target.y - tail.y;
                 tail.x += move_distance * tail_dx / distance;
                 tail.y += move_distance * tail_dy / distance;
             }
-           
         }
     }
 
@@ -123,23 +118,25 @@ export class Snake extends AcGameObject {
             ctx.arc(cell.x * L, cell.y * L, L / 2 * 0.8, 0, Math.PI * 2);
             ctx.fill();
         }
-        for(let i = 1;i<this.cells.length;++i){
-            const a = this.cells[i-1],b = this.cells[i];
-            if(Math.abs(a.x-b.x)<this.eps && Math.abs(a.y-b.y)<this.eps)
+
+        for (let i = 1; i < this.cells.length; i ++ ) {
+            const a = this.cells[i - 1], b = this.cells[i];
+            if (Math.abs(a.x - b.x) < this.eps && Math.abs(a.y - b.y) < this.eps)
                 continue;
-            if(Math.abs(a.x-b.x)<this.eps){
-                ctx.fillRect((a.x-0.4)*L,Math.min(a.y,b.y)*L,L*0.8,Math.abs(a.y-b.y)*L);
-            }else{
-                ctx.fillRect(Math.min(a.x,b.x)*L,(a.y-0.4)*L,Math.abs(a.x-b.x)*L,L*0.8);
+            if (Math.abs(a.x - b.x) < this.eps) {
+                ctx.fillRect((a.x - 0.4) * L, Math.min(a.y, b.y) * L, L * 0.8, Math.abs(a.y - b.y) * L);
+            } else {
+                ctx.fillRect(Math.min(a.x, b.x) * L, (a.y - 0.4) * L, Math.abs(a.x - b.x) * L, L * 0.8);
             }
         }
-        
+
         ctx.fillStyle = "black";
-        for(let i = 0;i <2 ;++i){
-            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i]*0.15)*L;
-            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i]*0.15)*L;
+        for (let i = 0; i < 2; i ++ ) {
+            const eye_x = (this.cells[0].x + this.eye_dx[this.eye_direction][i] * 0.15) * L;
+            const eye_y = (this.cells[0].y + this.eye_dy[this.eye_direction][i] * 0.15) * L;
+
             ctx.beginPath();
-            ctx.arc(eye_x,eye_y,L*0.05,0,Math.PI*2);
+            ctx.arc(eye_x, eye_y, L * 0.05, 0, Math.PI * 2);
             ctx.fill();
         }
     }
